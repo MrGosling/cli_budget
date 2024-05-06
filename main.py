0ed5a4c66f76a147c1630b2c8f77a31fb1fc7159
Gregory Shevchuk <2097255@mail.ru>
2024-05-06 12:33:58 +0300

    Search method added, stable

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from datetime import datetime
 import json
 
+class BudgetService:
+    
+    @staticmethod
+    def get_list(filename):
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            raise ('Файл не найден!')
+        return data
+
+    @staticmethod
+    def load(filename='file.json'):
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            data = []
+        return data
 
 class Budget:
     
-    def __init__(self, category, amount, description):
-        self.date: datetime = datetime.now()
+    def __init__(self, category, amount, description, date = datetime.now()):
+        self.date: datetime = date
         self.category: str = category
         self.amount: int = amount
         self.description: str = description
@@ -19,14 +38,36 @@ class Budget:
         }
         return data
 
-    def save(self, filename='file.txt'):
-        data = self.__str__()
+    def _get_balance(self, budget_list):
+        # сортировка по категории доходы/расходы, разница между доходами и расходами
+        pass
 
-        with open(filename, 'a+') as file:
-            if file.tell() > 0:
-                file.write('\n')
-            json.dump(data, file, ensure_ascii=False, indent=4)
-            
-# from main import Budget
-# b = Budget(category='1', amount=20, description='new')
-# b.save('file.json')
+    @staticmethod
+    def find(filename='file.json', *args):
+    # def find(self, filename='file.json', category=None, date=None, amount=None):
+        data: list[dict] = BudgetService.get_list(filename)
+        list_to_find = [x for x in args if x != None]
+        result = []
+        for item in data:            
+            if all(key in item.values() for key in list_to_find):
+                result.append(item)
+        print(result)
+        return result
+
+    def patch(self, category, amount, description):
+        # изменение существующего объекта
+        pass
+
+    def save(self, filename='file.json'):
+        data = self.__str__()
+        budget_list = BudgetService.load(filename)
+        budget_list.append(data)
+        with open(filename, 'w') as file:
+            json.dump(budget_list, file, ensure_ascii=False, indent=1)
+
+if __name__ == '__main__':
+    b = Budget(category='1', amount=20, description='new')
+    c = Budget(category='2', amount=30, description='1new')
+    c.save()
+    b.save()
+    Budget.find('file.json', '1', 20)
