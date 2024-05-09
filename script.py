aff4559c64a60eb7bc39e2f42b6aeb20a50af52e
Gregory Shevchuk <2097255@mail.ru>
2024-05-10 00:50:41 +0300

    Trying to enum category

--- script.py
+++ script.py
@@ -10,36 +10,41 @@ def set_filename():
     return FILE_NAME
 
 class Transaction:
-    
+    @classmethod
+    def get_transaction_type(cls, transaction_type: str) -> 'Transaction.TransactionType':
+        if transaction_type.title() == 'Доход':
+            return cls.TransactionType.INCOME
+        elif transaction_type.title() == 'Расход':
+            return cls.TransactionType.EXPENSE
+        else:
+            raise ValueError(
+                'Неправильная категория, можно выбрать Доход или Расход'
+            )
+
     class TransactionType(Enum):
-        INCOME = 'income'
-        EXPENSE = 'expense'
+        INCOME = 'Доход'
+        EXPENSE = 'Расход'
     
-    def __init__(self, date, category, amount: str, description):
-        self.date = date
-        self.category = category
-        self.amount = int(amount)
-        self.description = description
-        
-    def to_dict(self):
+    def __init__(self, date: str, category: str, amount: str, description: str):
+        self.date: str = date
+        self.category = self.get_transaction_type(category)
+        # self.category: str = category
+        self.amount: int = int(amount)
+        self.description: str = description
+
+    def to_dict(self) -> dict[str, str]:
         return {
             'Дата': self.date,
-            'Категория': self.category,
+            'Категория': self.category.value,
             'Сумма': str(self.amount),
             'Описание': self.description,
         }
 
 
 class BudgetService:
-    # @staticmethod
-    # def set_filename(filename):
-    #     return filename
 
     @staticmethod
     def load(filename):
-        # if filename is None:
-            # filename = FILE_NAME
-            # filename = set_filename()
         try:
             with open(filename, 'r') as file:
                 data = json.load(file)
@@ -51,8 +56,6 @@ class BudgetService:
 
     @staticmethod
     def write(data, filename):
-        # if filename is None:
-        #     filename = set_filename()
         try:
             with open(filename, 'w') as file:
                 json.dump(data, file, ensure_ascii=False, indent=4)
@@ -61,30 +64,29 @@ class BudgetService:
 
 
 class BudgetManager:
-    def __init__(self, data):
+    def __init__(self, data: list[Transaction]):
         self.data: list[Transaction] = data
 
-    def _get_index(self, date, category, amount, description):
+    def _get_index(self, date, category, amount, description) -> int | None:
         for index, obj in enumerate(self.data):
             if obj.date == date and obj.category == category and obj.amount == amount and obj.description == description:
                 return index
         print('Не удалось найти запись для обновления')
-        # raise ValueError('Не удалось найти объект для обновления')
 
-    def _save_data(self):
-        data_to_save = [transaction.to_dict() for transaction in self.data]
+    def _save_data(self) -> None:
+        data_to_save: list[dict[str, str]] = [transaction.to_dict() for transaction in self.data]
         Budget.save(data=data_to_save)
 
-    def create(self, date, category, amount, description):
+    def create(self, date, category, amount, description) -> str:
         new_entry = Transaction(date, category, amount, description)
         self.data.append(new_entry)
         self._save_data()
         return print(f'Создана новая запись: {new_entry.to_dict()}')
 
-    def filter(self, *args):
+    def filter(self, *args) -> str:
         if not self.data:
             raise FileNotFoundError('Файл не найден или в нём отсутствуют записи!')
-        result = []
+        result: list[dict[str, str]] = []
         for item in self.data:
             flag = True
             for arg in args:
@@ -98,13 +100,13 @@ class BudgetManager:
             print('По вашему запросу ничего не найдено')
         )
 
-    def get_balance(self):
+    def get_balance(self) -> str:
         if self.data == []:
             raise FileNotFoundError(
                 'Файл не найден или в нём отсутствуют записи!'
             )
-        income = 0
-        expenses = 0
+        income: int = 0
+        expenses: int = 0
         for item in self.data:
             if item.category == 'Доход':
                 income += item.amount
@@ -113,7 +115,7 @@ class BudgetManager:
         balance = income - expenses
         return print(f'Ваш баланс составляет {balance}')
 
-    def get_income(self):
+    def get_income(self) -> str:
         if self.data == []:
             raise FileNotFoundError(
                 'Файл не найден или в нём отсутствуют записи!'
@@ -124,22 +126,22 @@ class BudgetManager:
                 income += item.amount
         return print(f'Ваши доходы составляют {income}')
 
-    def get_expenses(self):
+    def get_expenses(self) -> str:
         if self.data == []:
             raise FileNotFoundError(
                 'Файл не найден или в нём отсутствуют записи!'
             )
-        expenses = 0
+        expenses: int = 0
         for item in self.data:
             if item.category == 'Расход':
                 expenses += item.amount
         return print(f'Ваши расходы составляют {expenses}')
 
     def update(
-        self, date, category, amount, description,
-        new_date, new_category, new_amount, new_description
-    ):
-        index = self._get_index(date, category, int(amount), description)
+        self, date: str, category: str, amount: str, description: str,
+        new_date: str, new_category: str, new_amount: str, new_description: str
+    ) -> str:
+        index: int | None = self._get_index(date, category, int(amount), description)
         if index is not None:
             self.data[index].date = new_date
             self.data[index].category = new_category
@@ -150,26 +152,20 @@ class BudgetManager:
 
 
 class Budget:
-    filename = set_filename()#'test_data.json'
-    list_of_transactions = BudgetService.load(filename)
-    data = []
+    filename: str = set_filename()
+    list_of_transactions: list[dict[str, str]] = BudgetService.load(filename)
+    data: list[Transaction] = []
     for item in list_of_transactions:
-        transaction = Transaction(date = item['Дата'], category=item['Категория'], amount=item['Сумма'], description=item['Описание'])
+        transaction = Transaction(
+            date=item['Дата'],
+            category=item['Категория'],
+            amount=item['Сумма'],
+            description=item['Описание']
+        )
         data.append(transaction)
     objects = BudgetManager(data)
     @classmethod
-    def save(cls, data):
-        BudgetService.write(data, filename=FILE_NAME)
+    def save(cls, data) -> None:
+        BudgetService.write(data, filename=set_filename())
 
-if __name__ == '__main__':
-    # print(Budget)
-    # print(Budget.objects.all())
-    # Budget.objects.filter('2024-05-03','Расход', '20')
-    Budget.objects.get_balance()
-    # Budget.objects().get_balance()
-    # Budget.objects.get_expenses()
-    # Budget.objects.get_income()
-    # Budget.objects.update(date='2024-05-03', category='Доход', amount=2000, description='new', new_date='2020-06-03', new_category='Доход', new_amount=5000, new_description='not_new',)
-    # Budget.objects.create(date='2024-05-03', category='Доход', amount=2000, description='new')
-    # Budget.all()
-    # print(Budget.objects.all())
+# if __name__ == '__main__':
